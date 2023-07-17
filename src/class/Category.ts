ea3d505a5deb1359f17da8c1772fd8a683ca5fd7
Mayopi <cyade007@gmail.com>
2023-07-17 15:56:53 +0700

    Category class

--- src/class/Category.ts
+++ src/class/Category.ts
@@ -2,6 +2,7 @@ import fs from "fs-extra";
 import { ICategory } from "../common/fetchCategory";
 import OrganizeLogger from "../logs/logFile";
 import sleep from "../common/sleep";
+import puppeteer from "puppeteer";
 
 class Category {
   private registeredCategory: ICategory[];
@@ -14,7 +15,7 @@ class Category {
     return this.registeredCategory;
   }
 
-  async writeCategory(data: ICategory[]): Promise<void> {
+  writeCategory = async (data: ICategory[]): Promise<void> => {
     const logger = new OrganizeLogger();
     try {
       fs.writeFileSync("src/data/category.json", JSON.stringify(data));
@@ -24,7 +25,55 @@ class Category {
       await sleep(2000);
       this.writeCategory(data);
     }
-  }
+  };
+
+  fetchCategory = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const logger = new OrganizeLogger();
+    const baseURL = "https://fileinfo.com/filetypes/";
+
+    try {
+      logger.info("Go to " + baseURL);
+      await Promise.all([page.goto(baseURL, { timeout: 0 }), page.waitForNavigation({ timeout: 0, waitUntil: "networkidle0" })]);
+
+      logger.info(`grabbing all category`);
+      let categoryGrouping: { href: string; text: string }[][] = await page.$$eval("#left article.browse div.buttongroup.cats", (divs) => {
+        return divs.map((div) => {
+          const links = Array.from(div.querySelectorAll("a"));
+          const hrefs = links.map((link) => ({ href: link.getAttribute("href").split("/").pop(), text: link.textContent }));
+          return hrefs;
+        });
+      });
+
+      for (const div of categoryGrouping) {
+        for (const link of div) {
+          if (link.href === "common") continue; // Skip "common" link
+          const category = new Category();
+          const registeredCategory: ICategory[] = category.categoryList;
+
+          // skip registered category
+          if (registeredCategory.some((item) => item.category == link.text)) continue;
+
+          logger.info(`Scraping ${link.text}, in ${baseURL}${link.href}`);
+          await Promise.all([page.goto(`${baseURL}${link.href}`, { timeout: 0 }), page.waitForNavigation({ timeout: 0, waitUntil: "networkidle0" })]);
+
+          logger.info(`grabbing all extension`);
+          const linkExtensions: string[] = await page.$$eval("td.extcol a", (elements) => {
+            return elements.map((element) => element.textContent);
+          });
+
+          registeredCategory.push({ extension: linkExtensions, category: link.text });
+
+          category.writeCategory(registeredCategory);
+        }
+      }
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      await browser.close();
+    }
+  };
 }
 
 export default Category;
--- src/sort/sortByCategory.ts
+++ src/sort/sortByCategory.ts
@@ -1,13 +1,8 @@
-import { IPrompt } from "../index";
-import CheckExtension from "../class/Extension";
-import readFiles from "../common/readFiles";
-import fetchCategory from "../common/fetchCategory";
-import CheckCategory from "../class/Category";
+import Category from "../class/Category";
 
-const sortByCategory = async (data: IPrompt) => {
-  const readData = readFiles(data.path);
-  const extensions = new CheckExtension(readData);
-  await fetchCategory();
+const sortByCategory = async () => {
+  const category = new Category();
+  await category.fetchCategory();
 };
 
 export default sortByCategory;
